5e35598b2e44f4fac45a9a32b6ed4b29cc797f90
octopus198 <linhtran190897@gmail.com>
2024-08-11 17:56:32 +0700

    add headers

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -61,9 +61,9 @@ const RegisterPage = () => {
         `${process.env.NEXT_PUBLIC_API_URL}/register`,
         {
           method: "POST",
-          // headers: {
-          //   "Content-Type": "application/json",
-          // },
+          headers: {
+            "Content-Type": "application/json",
+          },
           body: JSON.stringify(data),
         }
       );
